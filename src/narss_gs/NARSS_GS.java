ab62f41d2234a4f9e2c0bad64ca7c0446693b4fe
Moustafa Elgammal <moustafa.m.algammal@gmail.com>
2017-08-10 09:25:24 +0200

    Simple CRC achieved :)

--- src/narss_gs/NARSS_GS.java
+++ src/narss_gs/NARSS_GS.java
@@ -4,6 +4,11 @@ public class NARSS_GS {
 
     public static void main(String[] args) {
         new Database_connect().connect(); 
+       
+        int x = new CRC().GenerateChecksumCRC16(new int [] {5,6,8,7,01});
+        System.out.println("CRC "+ x);
     }
+    
+    
     
 }
